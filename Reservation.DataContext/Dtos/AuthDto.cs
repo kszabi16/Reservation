c535db9aef3f3d5c8707aac2377ff5bac769e9bf
kiss.szabolcs <kiss.szabolcs@gmail.com>
2026-04-09 16:08:15 +0200

    Dto-Update

--- Reservation.DataContext/Dtos/AuthDto.cs
+++ Reservation.DataContext/Dtos/AuthDto.cs
@@ -1,17 +1,25 @@
 using Reservation.DataContext.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Reservation.DataContext.Dtos
 {
     public class RegisterDto
     {
         public string Username { get; set; } = null!;
+        [Required(ErrorMessage = "Az email kötelező!")]
+        [EmailAddress(ErrorMessage = "Érvénytelen email formátum!")]
         public string Email { get; set; } = null!;
+        [Required]
+        [MinLength(6, ErrorMessage = "A jelszónak legalább 6 karakternek kell lennie!")]
         public string Password { get; set; } = null!;
     }
 
     public class LoginDto
     {
+        [Required(ErrorMessage = "Az email kötelező!")]
+        [EmailAddress(ErrorMessage = "Érvénytelen email formátum!")]
         public string Email { get; set; } = null!;
+        [Required]
         public string Password { get; set; } = null!;
     }
 
@@ -22,11 +30,5 @@ namespace Reservation.DataContext.Dtos
         public string Email { get; set; } = null!;
         public RoleType Role { get; set; }
         public string Token { get; set; } = null!;
-    }
-
-    public class ChangePasswordDto
-    {
-        public string CurrentPassword { get; set; } = null!;
-        public string NewPassword { get; set; } = null!;
     }
 }
